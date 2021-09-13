31e6277e28e901bb2c00272d0dac6c026b4e1758
H0R4T1U <horatiubanciu11@gmail.com>
2021-09-13 23:50:17 +0300

    4 projects

--- Nebula/views.py
+++ Nebula/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Project
 # Create your views here.
 def index(request):
-    projects = Project.objects.all().order_by('-stars')[::-1]
+    projects = Project.objects.all().order_by('-stars')[::4]
 
     context = {
         'projects':projects
