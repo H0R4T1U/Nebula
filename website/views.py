954a92b3f285d4197894023062d9bb43f09b67bc
H0R4T1U <horatiubanciu11@gmail.com>
2021-09-19 18:44:02 +0300

    error templates

---
+++ website/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+
+def error_400(request,  exception):
+        return render(request,'error/400.html', status=400)
+
+def error_403(request, exception):
+        return render(request,'error/403.html', status=403)
+        
+def error_404(request, exception):
+        return render(request,'error/404.html', status=404)
+
+def error_500(request,  exception):
+        return render(request,'error/500.html', status=500)
+
