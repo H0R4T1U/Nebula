b4ed51b4590f71c9e7efb22e05dfa1aeead8a050
H0R4T1U <horatiubanciu11@gmail.com>
2021-09-25 16:19:03 +0300

    added favicon.ico

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,14 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic.base import RedirectView
 
 
 
 urlpatterns = [
     path('',include('Nebula.urls')),
     path('admin/', admin.site.urls),
+    path("favicon.ico",RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")))
     
 ]
 
